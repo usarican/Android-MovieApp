193fafbc62d0fab533cbd6d2c9b91507e46caa3b
utkusarican <utku.sarican@rapsodo.com>
2022-09-26 09:51:50 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/moviesapplication/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.moviesapplication.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.moviesapplication.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
