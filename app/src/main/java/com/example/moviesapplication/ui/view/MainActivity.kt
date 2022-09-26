3f900864f6852ac4e5d04cad4d6ed2da0344b14a
utkusarican <utku.sarican@rapsodo.com>
2022-09-26 16:00:29 +0300

    UI Designs Added Movie Fragment and Movie Details Fragment Added Navigation Component

--- app/src/main/java/com/example/moviesapplication/ui/view/MainActivity.kt
+++ app/src/main/java/com/example/moviesapplication/ui/view/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.example.moviesapplication.ui
+package com.example.moviesapplication.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.moviesapplication.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
